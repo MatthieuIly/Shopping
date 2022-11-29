11952f45f059c58668f42d2f4af31980aa757c69
Matthieu Ily <sankokai2@hotmail.com>
2022-11-29 23:12:11 +0100

    feat: Travail sur le style de la page de liste des articles

--- resources/views/shopping/liste.blade.php
+++ resources/views/shopping/liste.blade.php
@@ -3,23 +3,28 @@
 @section('content')
 <h1>Shopping liste</h1>
 
-<div class="bg-white shadow-lg max-w-lg flex flex-col">
-    <ul class="list-none flex-1">
+<div class="bg-gray-200 shadow-lg max-w-lg flex flex-col">
+    <div class="card h-16 bg-gray-50 border border-gray mb-4 flex items-center">
+        <form action="{{ route('shopping.add') }}" method="post" 
+            class="grid grid-form-article">
+            @csrf
+            <input type="text" name="article" class="input"/>
+            <button type="submit" class="text-green-600 bg-green-100 px-2 rounded-full border border-green-600">Ajouter article</button>
+        </form>
+    </div>
+    
     @foreach($articles as $article)
-        <li class="my-4">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="article{{ $article->id }}">
-                <label class="form-check-label" for="article{{ $article->id }}">
-                    {{ $article->nom }}
-                </label>
-            </div>
-        </li>
+    <div class="card h-16 bg-gray-50 border border-gray flex flex-row">
+        <div class="card-text ml-4 mt-2">
+            <h1 class="text-2xl">{{ $article->nom }}</h1>
+            <p></p>
+        </div>
+        <div class="card-button flex items-center justify-end pr-2">
+            <button class="text-green-600 bg-green-100 px-2 rounded-full border border-green-600">Complete</button>
+            <button class="ml-2 text-red-600 bg-red-100 px-2 rounded-full border border-red-600">Delete</button>
+        </div>
+    </div>
     @endforeach
-    </ul>
-    <form action="{{ route('shopping.add') }}" method="post" class="flex-1">
-        @csrf
-        <input type="text" name="article" class="input"/>
-        <button type="submit" class="btn">Ajouter article</button>
-    </form>
+
 </div>
 @endsection
