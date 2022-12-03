eaf6515c8db09b72542e14dfd069e6292321df82
Matthieu Ily <sankokai2@hotmail.com>
2022-12-03 23:07:05 +0100

    feat: Ajoute les ressources Book

---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+        
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling(); // reals errors
+
+        $response = $this->post('/books', [
+            'title' => 'Cool book title',
+            'author' => 'Victor',
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+        // $this->withoutExceptionHandling(); // reals errors
+
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Victor',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+
+    }
+
+    /** @test */
+    public function a_author_is_required()
+    {
+        // $this->withoutExceptionHandling(); // reals errors
+
+        $response = $this->post('/books', [
+            'title' => 'Cool title',
+            'author' => '',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated() {
+        $this->withoutExceptionHandling(); // reals errors
+
+        $this->post('/books', [
+            'title' => 'Cool title',
+            'author' => 'Victor',
+        ]);
+
+        $book = Book::first();
+
+        $reponse = $this->patch('/books/' . $book->id, [
+            'title' => 'New title',
+            'author' => 'New author',
+        ]);
+
+        $this->assertEquals('New title', Book::first()->title);
+        $this->assertEquals('New author', Book::first()->author);
+    }
+}
