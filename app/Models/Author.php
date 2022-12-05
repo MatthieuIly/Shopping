3e9b2772addc2d7177e94f1b959d6e7b15a5d751
Matthieu Ily <sankokai2@hotmail.com>
2022-12-05 14:37:12 +0100

    feat: Add Author resources

---
+++ app/Models/Author.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $dates = ['dob'];
+
+    public function setDoAttribute($dob)
+    {
+        $this->attributes['dob'] = Carbon::parse($dob);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\ShoppingController;
 use App\Http\Controllers\TailwindcssController;
 
@@ -30,3 +31,5 @@ Route::get('/tailwindcss', [TailwindcssController::class, 'index'])->name('tailw
 Route::post('/books', [BooksController::class, 'store']);
 Route::patch('/books/{book}', [BooksController::class, 'update']);
 Route::delete('/books/{book}', [BooksController::class, 'delete']);
+
+Route::post('/author', [AuthorController::class, 'store']);
